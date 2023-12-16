a27a1c1d01b666702734f6f6d6613dc509b81ec2
Widhi Pieradita <widhipieradita@gmail.com>
2023-12-16 17:01:11 +0700

    add files for the project

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,25 @@
-let profile = document.querySelector(".profile");
-let profileName = profile.querySelector(".profile__name");
-let profileAbout = profile.querySelector(".profile__about");
+let profile = document.querySelector('.profile');
+let profileName = profile.querySelector('.profile__name');
+let profileAbout = profile.querySelector('.profile__about');
 
-let editButton = profile.querySelector(".profile__edit-button");
+let editButton = profile.querySelector('.profile__edit-button');
 
-let popup = document.querySelector(".popup");
-let closeButton = popup.querySelector(".popup__close");
-let formElement = popup.querySelector(".popup__edit");
-let popupEdit = popup.querySelector(".popup__edit");
-let inputName = popupEdit.querySelector(".popup__input_name");
-let inputAbout = popupEdit.querySelector(".popup__input_about");
+let popup = document.querySelector('.popup');
+let closeButton = popup.querySelector('.popup__close');
+let formElement = popup.querySelector('.popup__edit');
+let popupEdit = popup.querySelector('.popup__edit');
+let inputName = popupEdit.querySelector('.popup__input_name');
+let inputAbout = popupEdit.querySelector('.popup__input_about');
 
-
-let profileNameText = profileName.textContent;
-let profileAboutText = profileAbout.textContent;
 
 function handleProfileFormShow() {
-  popup.classList.add("popup_opened");
+  popup.classList.add('popup_opened');
   inputName.value = profileName.textContent;
   inputAbout.value = profileAbout.textContent;
 }
 
 function handleProfileFormHide() {
-  popup.classList.remove("popup_opened")
+  popup.classList.remove('popup_opened')
 }
 
 function handleProfileFormSubmit(evt) {
